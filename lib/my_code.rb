593c781ad72921bfc5d572dd1243481e76d2ca8e
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-17 10:02:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -24,14 +24,14 @@ def map_to_double (array)
   new_array =[]
 
   array.each { |element| new_array << element * 2  }
-  
+
   new_array
 end
 
 def map_to_square(array)
   new_array=[]
-  
+
   array.each { |element| new_array << element ** 2  }
-      
+
   new_array
 end
